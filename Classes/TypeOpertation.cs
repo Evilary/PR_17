7b0ef5d2a532e077d5321a8be69b751e76ace003
Pleshz <pleskovd96@gmail.com>
2026-01-31 13:03:18 +0500

    Реализованные классы. Чернышков

---
+++ Classes/TypeOpertation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintManagementSystem_Чернышков.Classes
+{
+    public class TypeOpertation
+    {
+        public int id {  get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public TypeOpertation(int _id, string _name , string _description) {
+        
+            this.id = _id;
+            this.name = _name;
+            this.description = _description;    
+    
+        }
+        public static List<TypeOpertation> AllTypeOperation() {
+        
+            List<TypeOpertation> allTypeOperation = new List<TypeOpertation> ();
+
+            allTypeOperation.Add(new TypeOpertation(1, "Печать", ""));
+            allTypeOperation.Add(new TypeOpertation(2, "Копия", ""));
+            allTypeOperation.Add(new TypeOpertation(3, "Сканирование", ""));
+            allTypeOperation.Add(new TypeOpertation(4, "Ризограф", ""));
+
+            return allTypeOperation;
+        
+        
+        }
+
+    }
+}
